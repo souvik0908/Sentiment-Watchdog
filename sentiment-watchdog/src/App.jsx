75e26a2dfa327c67f3748f5b3d76911a4b2b4222
Shivani Raj <shivani.raj.urs1105@gmail.com>
2025-07-21 17:20:29 +0530

    Create App.jsx

---
+++ sentiment-watchdog/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SentimentWatchdog from './SentimentWatchdog';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SentimentWatchdog />
+    </div>
+  );
+}
+
+export default App;
